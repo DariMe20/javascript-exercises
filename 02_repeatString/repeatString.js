bf32ba01aa01d5ffbe11cb29610f3c89fc0403ae
Daria <dariamesesan@yahoo.com>
2023-03-21 20:43:25 +0200

    Solved problem 2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
 
+const repeatString = function(text, n) {
+    if(n <0)
+        return "ERROR";
+    let newString='';
+    for(let i=0; i<n; i++)
+        newString = newString.concat("",text);
+    return newString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
 
+const reverseString = function(text) {
+    let reversedText='';
+
+    //mergem de la capat spre inceput 
+    for(let i=text.length-1; i>=0; i--)
+    {
+        //luam cate o litera de la capat spre inceput
+        let last_letter = text.slice(i, i+1);
+        //o adaugam in textul reversed
+        reversedText =reversedText.concat(last_letter);
+    }
+    return reversedText;
 };
 
 // Do not edit below this line
