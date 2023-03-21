4712bc4304b40088cfa49ea5a714a72069415d7c
Daria <dariamesesan@yahoo.com>
2023-03-21 20:47:07 +0200

    Solved problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperatureF) {
+  let tempC = ((temperatureF - 32) * 5) / 9;
+ 
+  if ((tempC % 1) == 0) //daca valoarea este intreaga, o returnez fara virgula 
+    return tempC;
+  else
+    return parseFloat(tempC.toFixed(1)); //utilizez functia toFixed() pentru a seta o singura zecimala dupa virgula
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperatureC) {
+  let tempF = (temperatureC * 9) / 5 + 32;
+  if ((tempF % 1) == 0) //daca valoarea este intreaga, o returnez fara virgula 
+  return tempF;
+else
+  //utilizez parseFloat pentru a converti stringul in float
+  return parseFloat(tempF.toFixed(1)); //utilizez functia toFixed() pentru a seta o singura zecimala dupa virgula
 };
 
 // Do not edit below this line
@@ -9,3 +21,5 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
